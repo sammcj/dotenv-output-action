e0cd3f002f439bd8a9953d8e7cfc7db15980f8fb
Sam McLeod <sammcj@users.noreply.github.com>
2022-11-29 16:16:48 +1100

    feat: add array splitting capability

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const dotenvAction = require('./dotenv_action');
 try {
   const dotenvFile = core.getInput('path');
   const logVariables = core.getInput('log-variables').toLowerCase() === 'true';
-  const maskVariables =
-    core.getInput('mask-variables').toLowerCase() === 'true';
-  const variables = dotenvAction(dotenvFile, logVariables);
+  const maskVariables = core.getInput('mask-variables').toLowerCase() === 'true';
+  const splitArrays = core.getInput('split-values').toLowerCase() === 'false';
+  const variables = dotenvAction(dotenvFile, logVariables, splitArrays);
 
   if (maskVariables) {
     for (const key in variables) {
@@ -17,9 +17,7 @@ try {
   if (logVariables) {
     console.log(variables);
   } else {
-    console.log(
-      `loaded ${Object.keys(variables).length} values into the environment`
-    );
+    console.log(`loaded ${Object.keys(variables).length} values into the environment`);
   }
 
   core.setOutput('generic', 'please check for actual outputs');
